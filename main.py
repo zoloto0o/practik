732de8dcf0b19792a9a2017d969e1ae8761fd00d
zoloto0o <144393792+zoloto0o@users.noreply.github.com>
2024-11-03 21:27:06 +0200

    Update main.py
    Запускаем бота и настраиваем диспетчер команд

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # main.py
+
+from telegram.ext import Updater
+from config import TELEGRAM_API_KEY
+from bot_handlers import setup_dispatcher
+
+def main() -> None:
+    """Запускает бота."""
+    updater = Updater(TELEGRAM_API_KEY)
+    dispatcher = updater.dispatcher
+
+    # Настраиваем обработчики
+    setup_dispatcher(dispatcher)
+
+    # Запуск бота
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
